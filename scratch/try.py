581c3b8192cce7ccf5589a28abc5b4bc097802eb
Chris Archibald <carchi8py@gmail.com>
2016-10-05 17:22:30 -0700

    Resize the image

--- scratch/try.py
+++ scratch/try.py
@@ -1,6 +1,7 @@
 import cv2
 
 img = cv2.imread("galaxy.jpg", 0)
-cv2.imshow("Galaxy", img)
+resized_image = cv2.resize(img, (1000,500))
+cv2.imshow("Galaxy", resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
