43a793621e529de53ac0ee90885f5f2ade2720cc
Chris Archibald <carchi8py@gmail.com>
2016-10-05 17:49:55 -0700

    Find faces

--- face_detection/findFace.py
+++ face_detection/findFace.py
@@ -2,11 +2,11 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img = cv2.imread("photo.jpg")
+img = cv2.imread("news.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray_img, 
-                                      scaleFactor=1.05,
+                                      scaleFactor=1.1,
                                       minNeighbors=5)
 
 for x, y, w, h in faces:
